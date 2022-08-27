0051ed2f5039efc6852629447e33441165dee597
Ji Yoon Park (JUNY) <99635366+OrangeChick@users.noreply.github.com>
2022-08-27 18:25:02 +0900

    Time: 100 ms (40.95%), Space: 42.3 MB (29.93%) - LeetHub

---
+++ 50-powx-n/50-powx-n.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    let answer = Math.pow(x, n);
+    
+    return answer.toFixed(5);
+    
+};
